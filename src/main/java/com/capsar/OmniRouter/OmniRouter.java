d781eba1e639c4bc5471d0c79287fd488b813c29
Adam Clouthier <adam.clouthier@gmail.com>
2014-07-29 11:35:27 -0400

    Successful Test 1
    Shows up in the Mods Menu

---
+++ src/main/java/com/capsar/OmniRouter/OmniRouter.java
@@ -0,0 +1,12 @@
+package com.capsar.OmniRouter;
+
+/**
+ * Created by adamc on 7/29/2014.
+ */
+import cpw.mods.fml.common.Mod;
+@Mod(modid = "OmniRouter", version = "1.7.10-0.1", name = "Omni Router")
+public class OmniRouter
+{
+
+
+}
