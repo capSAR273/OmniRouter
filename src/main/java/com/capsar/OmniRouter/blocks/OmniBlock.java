ee93f187c259e3724a5172eda4394defbba1033f
Adam Clouthier <adam.clouthier@gmail.com>
2014-07-30 14:00:33 -0400

    Created en_US.lang

---
+++ src/main/java/com/capsar/OmniRouter/blocks/OmniBlock.java
@@ -0,0 +1,47 @@
+package com.capsar.OmniRouter.blocks;
+
+import com.capsar.OmniRouter.OmniRouter;
+import com.capsar.OmniRouter.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Created by adamc on 7/30/2014.
+ */
+public class OmniBlock extends Block
+{
+    public OmniBlock(Material material)
+    {
+        super(material);
+    }
+    public OmniBlock()
+    {
+        this(Material.rock);
+    }
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    public String getUnlocalizedName(ItemStack itemStack)
+    {
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+        //tile.modid:blockname.name
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".")+ 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
--- src/main/java/com/capsar/OmniRouter/blocks/simpleRouter.java
+++ src/main/java/com/capsar/OmniRouter/blocks/simpleRouter.java
@@ -1,49 +1,19 @@
 package com.capsar.OmniRouter.blocks;
 
 import com.capsar.OmniRouter.creativetab.CreativeTabOmni;
-import com.capsar.OmniRouter.reference.Reference;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import com.capsar.OmniRouter.blocks.OmniBlock;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.item.ItemStack;
-import net.minecraft.block.Block;
 
 /**
  * Created by adamc on 7/29/2014.
  */
-public class simpleRouter extends Block
+public class simpleRouter extends OmniBlock
 {
     public simpleRouter()
     {
-        super(Material.rock);
-        //this.getUnlocalizedName("simpleRouter");
-        //this.getTextureName(Reference.MOD_ID + ":" + "simpleRouter");
+        super();
         this.setCreativeTab(CreativeTabOmni.OmniRouter);
-        this.setBlockName("Simple Router");
+        this.setBlockName("simplerouter");
     }
-
-    @Override
-    public String getUnlocalizedName()
-    {
-        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
-    }
-
-    public String getUnlocalizedName(ItemStack itemStack)
-    {
-        return String.format("item.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void registerIcons(IIconRegister iconRegister)
-    {
-        blockIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".")+ 1));
-    }
-
-    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
-    {
-        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
-    }
-
 
 }
